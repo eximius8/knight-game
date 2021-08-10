b2b41bd0d93f226d8282c0c70533dd927b37c728
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-08-11 01:05:46 +0300

    Создал абстрактный класс для стратегии.

--- abstract_classes.py
+++ abstract_classes.py
@@ -1,5 +1,6 @@
 """Классы, от которых наследуются остальные классы игры."""
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class AbstractEncounter(ABC):
@@ -17,30 +18,34 @@ class AbstractEncounter(ABC):
 
 
 class AbstractCreature(ABC):
+    """Класс для героев игры (игроков и монстров)."""
 
-    def __init__(self, creature_type, hp, features):
+    def __init__(self, creature_type: int, hp: int, features: dict):
+        # тип игрока
         self.creature_type = creature_type
         names = {1: "воин", 2: "лучник", 3: "маг"}
         self.name = names[self.creature_type]
+        # жизни игрока
         self.hp = hp
+        # артефакты игрока (например оружее, тотем и т.п.)
         self.features = features
 
-    def add_feature(self, feature):
+    def add_feature(self, feature: Any):
+        """Добавить артефакт к объекту."""
         self.features[feature.code] = feature
 
 
 class AbstractCreatureFactory(ABC):
+    """Фабрика производства героев."""
 
     @abstractmethod
     def create_creature(self):
-        """
-        Note that the Creator may also provide some default implementation of
-        the factory method.
-        """
+        """Метод для создания героев."""
         pass
 
 
 class AbstractFeatureFactory(ABC):
+    """Фабрика производства артефактов."""
 
     @abstractmethod
     def create_random_feature(self):
@@ -53,4 +58,18 @@ class AbstractFeatureFactory(ABC):
 class AbstractAttackStrategy(ABC):
     """Стратегия аттаки."""
 
-    pass
+    @abstractmethod
+    def get_available_weapons(self):
+        """Получение доступного оружия игрока (монстра).
+        возвращается список из доступных артефактов"""
+        pass
+
+    @abstractmethod
+    def choose_weapon(self):
+        """Выбор оружия для применения игроком.
+        Если оружие только одно, оно выбирается автоматически."""
+        pass
+
+    def attack(self):
+        """Атака игрока (монстра) на соперника."""
+        pass
